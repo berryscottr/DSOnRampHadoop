b6f032dffe6c69a0d353bb1617c7530b7bad918f
berryscottr <berryscottr@gmail.com>
2022-02-13 01:18:48 -0500

    ass 2 complete

--- Assignment_2/CCFinalRound.py
+++ Assignment_2/CCFinalRound.py
@@ -1,17 +1,29 @@
 class Graph:
-    def __init__(self, input_dict):
+    def __init__(self, conns):
         self.vertices = len(input_dict)
-        self.connections = input_dict
-        self.walks = self.create_walks()
+        self.connections = conns
+        self.walks = self.map_reduce()
 
-    def create_walks(self):
-        walks = []
+    def map_reduce(self):
         # mapper
-        for k, v in input_dict:
-            walks.append(k)
-
+        walks = []
+        for k, v in self.connections.items():
+            walk = []
+            walk.append(k)
+            for vals in v:
+                walk.append(vals)
+            for w in walk:
+                for vv in self.connections[w]:
+                    if vv not in walk:
+                        walk.append(vv)
+            walks.append(walk)
         # reducer
-        return walks
+        walks_reduced = []
+        for walk_index in range(0, len(walks)):
+            walk = walks[walk_index]
+            if walk not in walks_reduced:
+                walks_reduced.append(walk)
+        return walks_reduced
 
 
 def input2dict(input_txt_lines):
@@ -21,8 +33,10 @@ def input2dict(input_txt_lines):
     input_values = [val[1] for val in input_lines]
     input_values = [val.split(",") for val in input_values]
     lines_dict = {}
-    for i in range(0, len(input_keys)):
-        lines_dict[i] = [int(val) for val in input_values[i]]
+    val_index = 0
+    for i in input_keys:
+        lines_dict[i] = [int(val) for val in input_values[val_index]]
+        val_index += 1
     return lines_dict
 
 
@@ -32,12 +46,14 @@ if __name__ == "__main__":
     input_dict = input2dict(input_txt_lines)
     # Initialize graph object with all vertices as not visited
     g = Graph(input_dict)
-    # Add every connection from input
-    # for input_key in input_dict:
-    #     values = input_dict[input_key]
-    #     for value in values:
-    #         g.add_edge(input_key, value)
-    # Gather connected components
-    # connected_components = g.connected_components()
-    print("Following are connected components")
-    # print(connected_components)
+    # output to file
+    with open("output.txt", "w") as output_file:
+        output_text = ""
+        for walk in g.walks:
+            walk_text = "{}\t{}\n".format(
+                str(walk[0]),
+                str(walk[1:]).replace("[", "", 1).replace("]", "", 1).replace(" ", "", -1)
+            )
+            output_text += walk_text
+        output_file.write(output_text)
+    print("Connected Components\n{}".format(output_text))
